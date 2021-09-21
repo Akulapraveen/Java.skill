e3872cde466acbbd7837981aa309582016bea96e
Akulapraveen <89966784+Akulapraveen@users.noreply.github.com>
2021-09-21 09:59:02 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,12 @@
+class Palindrome
+{
+  public static void main(String args[])
+    {
+      String str1="Hello World";
+      String str2="Hello World";
+      if(str1==str2)
+       System.out.println("palindrome");
+      else    
+       System.out.println("not palindrome");   
+     }
+}
