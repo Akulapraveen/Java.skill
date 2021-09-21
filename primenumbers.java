3e54fb89b798c361163ee8a85c050050fd754d11
Akulapraveen <89966784+Akulapraveen@users.noreply.github.com>
2021-09-21 10:42:39 +0530

    Add files via upload

---
+++ primenumbers.java
@@ -0,0 +1,17 @@
+class primenumbers
+{
+   public static void main(String args[])
+    {
+        int num=Integer.parseInt(args[0]);
+        int count=0;
+        for(int i=2;i<=Math.sqrt(num);i++)
+            {
+             if (num%i==0)
+                 count++;
+             }
+        if (count==0)
+            System.out.println("Is Prime");
+        else
+            System.out.println("Not Prime");
+    }
+}
